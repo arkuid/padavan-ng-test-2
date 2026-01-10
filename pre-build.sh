8200c059f5b46cc9922dc8e520b204d4eb42d27b
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-11 02:09:34 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,8 @@
 
-sed -i 's|SRC_VER = 72\.[0-9]\+|SRC_VER = 72.7|' trunk/user/nfqws/Makefile
-sed -i 's|SRC_VER = 0\.8\.[0-9]\+|SRC_VER = 0.8.3|' trunk/user/nfqws2/Makefile
+sed -i 's|SRC_VER = 72\.[0-9]\+|SRC_VER = 72.7|' padavan-ng/trunk/user/nfqws/Makefile
+sed -i 's|SRC_VER = 0\.8\.[0-9]\+|SRC_VER = 0.8.3|' padavan-ng/trunk/user/nfqws2/Makefile
 
-find /trunk/user/nfqws -name "zapret-*.tar.gz"
-find /trunk/user/nfqws -name "zapret-*.tar.gz" -exec rm {} \;
-find /trunk/user/nfqws2 -name "zapret2-*.tar.gz"
-find /trunk/user/nfqws2 -name "zapret2-*.tar.gz" -exec rm {} \;
+find padavan-ng/trunk/user/nfqws -name "zapret-*.tar.gz"
+find padavan-ng/trunk/user/nfqws -name "zapret-*.tar.gz" -exec rm {} \;
+find padavan-ng/trunk/user/nfqws2 -name "zapret2-*.tar.gz"
+find padavan-ng/trunk/user/nfqws2 -name "zapret2-*.tar.gz" -exec rm {} \;
