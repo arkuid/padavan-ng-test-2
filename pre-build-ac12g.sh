7b70ef4183e60f40f3f156419ec68e676e235a7a
arkuid <149357139+arkuid@users.noreply.github.com>
2026-07-25 01:48:03 +0500

    Update pre-build-ac12g.sh

--- pre-build-ac12g.sh
+++ pre-build-ac12g.sh
@@ -1,2 +1,2 @@
-sed -i 's/SRC_VER = 1.0.2/SRC_VER = 1.0.3/' padavan-ng/trunk/user/zapret/zapret2/Makefile && rm padavan-ng/trunk/user/zapret/zapret2/zapret2-1.0.2.tar.gz
+#sed -i 's/SRC_VER = 1.0.2/SRC_VER = 1.0.3/' padavan-ng/trunk/user/zapret/zapret2/Makefile && rm padavan-ng/trunk/user/zapret/zapret2/zapret2-1.0.2.tar.gz
 sed -i '/CONFIG_FIRMWARE_INCLUDE_AMNEZIAWG/,/endif/ s/SRC_VER = 1.0.20260223/SRC_VER = 1.0.20260618-2/' padavan-ng/trunk/user/wireguard/Makefile && rm -f padavan-ng/trunk/user/wireguard/amneziawg-tools-1.0.20260223.tar.gz
